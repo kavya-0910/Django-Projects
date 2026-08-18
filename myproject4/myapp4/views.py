9ed1715a24cd94447d5d96c2e78167bcd69a5aa4
kavya-0910 <nameisnani2209@gmail.com>
2026-08-18 21:21:07 +0530

    Add files via upload

---
+++ myproject4/myapp4/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import request,HttpResponse
+# Create your views here.
+def indexPage(request):
+    return render(request,"index.html")
+def aboutPage(request):
+    return render(request,"about.html")
+def contactPage(request):
+    return render(request,"contact.html")
+def loginPage(request):
+    return render(request,"login.html")
